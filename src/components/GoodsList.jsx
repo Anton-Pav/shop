3b174c21a57a2922a5b548bdc82bf30dc51da2d9
Anton <ant.pavlov93@yandex.ru>
2022-09-09 18:52:47 +0300

    вывод списка товаров

---
+++ src/components/GoodsList.jsx
@@ -0,0 +1,13 @@
+import {GoodsItem} from "./GoodsItem";
+
+function GoodsList(props) {
+    const {goods = []} = props
+    if (!goods.length) {
+        return <h3>Nothing</h3>
+    }
+    return <div className="goods">
+        {goods.map(item => <GoodsItem key={item.id} {...item}/>)}
+    </div>
+}
+
+export {GoodsList}
